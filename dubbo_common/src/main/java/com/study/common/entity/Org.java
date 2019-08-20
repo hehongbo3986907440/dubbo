33e9a9c5bbc2eb265c4b153096cf769c0f60ce18
象征 <hehongbo3986907440@163.com>
2019-08-20 17:39:53 +0800

    组织类

---
+++ dubbo_common/src/main/java/com/study/common/entity/Org.java
@@ -0,0 +1,33 @@
+package com.study.common.entity;
+
+import java.io.Serializable;
+
+public class Org implements Serializable {
+    private String id;
+    private String name;
+    private String parent;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+}
